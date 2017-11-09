290ac192a9f8715f967d17464b4d4944ae5cbc22
Kurt <kurtrm@gmail.com>
2017-11-09 12:39:29 -0800

    Worked on linked list. Made a few changes.

---
+++ src/linked_list.py
@@ -0,0 +1,90 @@
+"""Write an implementation of linked lists in Python."""
+
+
+class Node(object):
+    """Node for use in a linked list."""
+
+    def __init__(self, data, next_node=None):
+        """Initialize a node in linked list."""
+        self.data = data
+        self.next_node = next_node
+
+
+class LinkedList(object):
+    """Linked version of a list."""
+
+    def __init__(self, iterable=None):
+        """Initialization rules for linked list."""
+        self._length = 0
+        self.head = None
+        if isinstance(iterable, (str, list, tuple)):
+            for item in iterable:
+                self.push(item)
+            self._length = len(iterable)
+        elif iterable is not None:
+            raise TypeError('Please give an iterable or don\'t type anything.')
+
+    def push(self, val):
+        """Push the new value to the head of the linked list."""
+        new_node = Node(val, self.head)
+        self._length += 1
+        self.head = new_node
+
+    def pop(self):
+        """Remove the head of the linked list."""
+        if not self.head:
+            raise IndexError(
+                'There\'s nothing to remove from the linked list.')
+        popped = self.head
+        self.head = self.head.next_node
+        popped.next_node = None
+        self._length -= 1
+        return popped.data
+
+    def size(self):
+        """Return the length of the linked list."""
+        return self._length
+
+    def __len__(self):
+        """Interact with built-in len() function."""
+        return self.size()
+
+    def search(self, val):
+        """Search the nodes for the value provided."""
+        current_node = self.head
+        while current_node is not None:
+            if current_node.data == val:
+                return current_node.data
+            current_node = current_node.next_node
+
+    def remove(self, val):
+        """Remove node from anywhere in the linked list with the given value."""
+        if self.head.data != val:
+            current_node = self.head
+            while current_node.next_node is not None:
+                if current_node.next_node.data == val:
+                    current_node.next_node = current_node.next_node.next_node
+                    break
+                current_node = current_node.next_node
+            else:
+                raise ValueError('Value not found.')
+        else:
+            self.head = self.head.next_node
+        self._length -= 1
+
+    def display(self):
+        """Present a visual representation of the linked list."""
+        node = self.head
+        display_str = ' '
+        while node is not None:
+                display_str += ' '
+                display_str += '\''
+                display_str += str(node.data)
+                display_str += '\''
+                node = node.next_node
+        display_str = ', '.join(display_str.split())
+        return "({})".format(display_str)
+
+    def __str__(self):
+        """Interact with built-in print function."""
+        return self.display()
---
+++ src/tests/test_linked_list.py
@@ -0,0 +1,157 @@
+"""Test our linked_list implementation."""
+
+
+import pytest
+
+
+@pytest.fixture
+def linked_list():
+    """Instantiate a  for testing."""
+    from ..linked_list import LinkedList
+    linked_list = LinkedList()
+    return linked_list
+
+
+def test_size(linked_list):
+    """Verify size works."""
+    linked_list.push('True')
+    linked_list.push(12)
+    linked_list.push(True)
+    assert linked_list.size() == 3
+
+
+def test_size_pop(linked_list):
+    """Verify size works."""
+    linked_list.push('True')
+    linked_list.push(12)
+    linked_list.push(True)
+    linked_list.pop()
+    assert linked_list.size() == 2
+
+
+def test_linked_list_length(linked_list):
+    """Verify we can return the length of the linked list."""
+    linked_list.push(3)
+    linked_list.push(4)
+    linked_list.push(5)
+    linked_list.pop()
+    assert len(linked_list) == 2
+
+
+def test_linked_list_head(linked_list):
+    """Test our linked_list class."""
+    assert hasattr(linked_list, 'head')
+
+
+def test_iterable_linked_list():
+    """Test values of initialized linked list."""
+    from ..linked_list import LinkedList
+    linked_list = LinkedList([1, 2, 3])
+    assert (len(linked_list), linked_list.head.data) == (3, 3)
+
+
+def test_iterable_linked_list_exception():
+    """Ensure exception raised if a non-iterable is passed as argument."""
+    from ..linked_list import LinkedList
+    with pytest.raises(TypeError):
+        new_link = LinkedList(1)
+
+
+def test_linked_list_push(linked_list):
+    """Test that push makes a new head of the list."""
+    linked_list.push(5)
+    assert linked_list.head.data == 5
+
+
+def test_linked_list_push_three(linked_list):
+    """Test attributes with three items in the linked list."""
+    linked_list.push(5)
+    linked_list.push(3)
+    linked_list.push(2)
+    assert (linked_list.head.data,
+            linked_list.head.next_node.data,
+            linked_list.head.next_node.next_node.data) == (2, 3, 5)
+
+
+def test_linked_list_popped(linked_list):
+    """Test pop method."""
+    linked_list.push(5)
+    linked_list.push(2)
+    assert (linked_list.pop(),
+            linked_list.head.data,
+            linked_list.pop()) == (2, 5, 5)
+
+
+def test_linked_list_popped_exception(linked_list):
+    """Raise exception if empty."""
+    with pytest.raises(IndexError):
+        linked_list.pop()
+
+
+def test_linked_list_search(linked_list):
+    """Test the linked list search method."""
+    from ..linked_list import LinkedList
+    obj_1 = 'apple'
+    obj_2 = 45
+    obj_3 = ['stringy']
+    linked_list = LinkedList([obj_1, obj_2, obj_3])
+    assert (linked_list.search(45),
+            linked_list.search(['stringy']),
+            linked_list.search(obj_1),
+            linked_list.search(47)) == (obj_2, obj_3, 'apple', None)
+
+
+def test_linked_list_remove_phase_one():
+    """Test proper remove functionality."""
+    from ..linked_list import LinkedList
+    new_list = LinkedList([1, 2, 3, 4])
+    new_list.remove(1)
+    assert new_list.search(1) is None
+
+
+def test_linked_list_remove_phase_two():
+    """Test proper remove functionality."""
+    from ..linked_list import LinkedList
+    new_list = LinkedList([1, 2, 3, 4])
+    new_list.remove(1)
+    new_list.remove(4)
+    assert (new_list.search(4),
+            new_list.search(3)) == (None, new_list.head.data)
+
+
+def test_linked_list_remove_phase_three():
+    """Test proper remove functionality."""
+    from ..linked_list import LinkedList
+    new_list = LinkedList([1, 2, 3, 4])
+    new_list.remove(1)
+    new_list.remove(4)
+    new_list.push(5)
+    new_list.remove(3)
+    assert (new_list.search(3),
+            new_list.search(5)) == (None, new_list.head.data)
+
+
+def test_linked_list_remove_phase_three_len():
+    """Test proper remove functionality."""
+    from ..linked_list import LinkedList
+    new_list = LinkedList([1, 2, 3, 4])
+    new_list.remove(1)
+    new_list.remove(4)
+    new_list.push(5)
+    new_list.remove(3)
+    assert len(new_list) == 2
+
+
+def test_linked_list_remove_exception():
+    """Test raises exception if value can't be removed."""
+    from ..linked_list import LinkedList
+    new_linked_list = LinkedList((1, 'Trip', True))
+    with pytest.raises(ValueError):
+        new_linked_list.remove('this')
+
+
+def test_linked_list_display():
+    """Our display list."""
+    from ..linked_list import LinkedList
+    new_list = LinkedList([1, 2, 3, 4])
+    assert new_list.display() == "('4', '3', '2', '1')"
