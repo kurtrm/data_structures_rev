58021698cd79ddef55277d727534a924032af2d6
kurt <kurtrm@gmail.com>
2017-11-26 18:12:56 -0800

    Added deque rev. Simple, need to do readme.

---
+++ src/deque.py
@@ -0,0 +1,42 @@
+"""Deque Composition."""
+from data_structures_rev.src.dll import DoubleLinkedList
+
+
+class Deque:
+    """Class deque for deque composition."""
+
+    def __init__(self):
+        """Init for deque."""
+        self._new_dll = DoubleLinkedList()
+
+    def append(self, val):
+        """Enqueue function for queue pushes value to head."""
+        return self._new_dll.push(val)
+
+    def appendleft(self, val):
+        """Append vals to the front of deque."""
+        return self._new_dll.append(val)
+
+    def pop(self):
+        """Pop from dll."""
+        return self._new_dll.pop()
+
+    def popleft(self):
+        """Shift for popleft."""
+        return self._new_dll.shift()
+
+    def peek(self):
+        """Return a new value without dequeuing it."""
+        return self._new_dll.head.data
+
+    def peekleft(self):
+        """Return a new value from the left without dequeing it."""
+        return self._new_dll.tail.data
+
+    def size(self):
+        """Length function for the queue."""
+        return self._new_dll.size()
+
+    def __len__(self):
+        """Return's size of deque."""
+        return self._new_dll.size()
