2a785bad9c8d0ba2f41dc835673d8c7f3d475f8b
kurt <kurtrm@gmail.com>
2018-01-16 17:58:05 -0800

    Ran the script on a list of 10000 integers. Took a long time, so I'm going to copy this notebook and save it for my presentation.

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,4 +1,5 @@
 """Classic bubble sort implementation."""
+import random
 
 
 def bubble_sort(lineup):
@@ -29,3 +30,49 @@ def bubble_sort(lineup):
                     lineup[prev], lineup[idx] = lineup[idx], lineup[prev]
         size -= 1
     return lineup
+
+
+def noisy_bubble_sort(lineup, noise_level=5):
+    """
+    Sort the iterable using the Bubble sort method.
+    params
+    -------
+    lineup: list, tuple, or str
+
+    return
+    ------
+    lineup: list, tuple, or str (in place)
+
+    Same as the previous bubble_sort implementation except adjusts the given variable
+    by a noise percentage at the time of comparison.
+    """
+    if not isinstance(lineup, (list, tuple, str)):
+        raise TypeError("Input must be either list, tuple, or str")
+
+    size = len(lineup)
+    while size > 0:
+        for idx, current in enumerate(lineup[:size]):
+            if idx:
+                prev = idx - 1
+                prev_noisy = lineup[prev] + noise(noise_level)
+                current_noisy = current + noise(noise_level)
+                if prev_noisy > current_noisy:
+                    lineup[prev], lineup[idx] = lineup[idx], lineup[prev]
+        size -= 1
+    return lineup
+
+
+def noise(level=5):
+    """
+    Parameters
+    ----------
+    {integer} : Threshold for the randomized variability
+
+    Returns
+    -------
+    {integer} : Randomly generated interval [-5, 5]
+    """
+    if level >= 0 and level <= 100:
+        return random.randint(-level, level + 1)
+    else:
+        raise ValueError('Enter a value between 0 and 100')
