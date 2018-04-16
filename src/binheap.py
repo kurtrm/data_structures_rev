8e9fdf0964824a6f98911b2763f43e36c3c05136
Kurt Maurer <kurtrm@gmail.com>
2018-04-15 19:14:27 -0700

    Added type hints to methods.

--- src/binheap.py
+++ src/binheap.py
@@ -37,9 +37,6 @@ class BinHeap:
 
     def pop(self) -> None:
         """Pop the min value from the heap, return it, and resort the heap."""
-        if len(self._iterable) == 0:
-            raise TypeError(
-                'Cannot pop from empty heap.')
         popped = self._iterable.pop(0)
         self.heapify(self._iterable)
         return popped
