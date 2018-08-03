95e0fe663ba02609cace45367b2e289c619ef409
Kurt Maurer <kurtrm@gmail.com>
2018-08-03 14:45:41 -0700

    Redoing BinHeap for further use in priorityq.

--- src/binheap.py
+++ src/binheap.py
@@ -9,32 +9,42 @@ class BinHeap:
     def __init__(self, iterable: Union[None, Iterable]=None) -> None:
         """Initialize an empty min heap."""
         try:
-            self._iterable = self.heapify(iterable)
+            for num in iterable:
+                self.push(num)
         except TypeError:
             if iterable is None:
                 self._iterable = []
             else:
                 raise ValueError('Argument must be an iterable')
 
-    def heapify(self, iterable: list) -> list:
-        """Function that will be used in init and other methods."""
-        for item in iterable[::-1]:
-            idx = iterable.index(item)
-            parent = (idx - 1) // 2
-            while idx > 0 and iterable[idx] < iterable[parent]:
-                iterable[parent], iterable[idx] = iterable[idx], iterable[parent]
-                idx = parent
-                parent = (idx - 1) // 2
+    # def heapify(self, iterable: list) -> list:
+    #     """Function that will be used in init and other methods."""
+    #     for item in reversed(iterable):
+    #         # import pdb; pdb.set_trace()
+    #         idx = iterable.index(item)
+    #         parent = (idx - 1) // 2
+    #         while idx > 0 and iterable[idx] < iterable[parent]:
+    #             iterable[parent], iterable[idx] = iterable[idx], iterable[parent]
+    #             idx = parent
+    #             parent = (idx - 1) // 2
 
-        return iterable
+    #     return iterable
+
+    def heapify(self):
+        """Function that will be used in init and other methods."""
+        idx = len(self._iterable) - 1
+        parent = (idx - 1) // 2
+        while idx > 0 and self._iterable[idx] < self._iterable[parent]:
+            self._iterable[parent], self._iterable[idx] = self._iterable[idx], self._iterable[parent]
+            idx = parent
+            parent = (idx - 1) // 2
 
     def push(self, val: Union[int, float]) -> None:
         """Push a value onto the heap."""
         if not isinstance(val, Number):
-            raise TypeError(
-                'Argument must be an integer or float')
+            raise TypeError('Argument must be an integer or float')
         self._iterable.append(val)
-        self._iterable = self.heapify(self._iterable)
+        self.heapify()
 
     def pop(self) -> None:
         """Pop the min value from the heap, return it, and resort the heap."""
