2a3a76a5276cc121748ba268cfc67e606e74ea64
Kurt Maurer <kurtrm@gmail.com>
2018-08-07 14:17:25 -0700

    Brought over changes from other branch.

--- src/binheap.py
+++ src/binheap.py
@@ -18,7 +18,7 @@ class BinHeap:
             else:
                 raise ValueError('Argument must be an iterable')
 
-    def _sort_down(self, index=0):
+    def _sort_down(self, index=0) -> None:
         """
         Starting from the top position, sort down the items in the list to get the max at the top.
         """
@@ -34,7 +34,7 @@ class BinHeap:
         except IndexError:
             pass
 
-    def heapify(self):
+    def heapify(self) -> None:
         """Function that will be used in init and other methods."""
         idx = len(self._iterable) - 1
         parent = (idx - 1) // 2
@@ -56,6 +56,7 @@ class BinHeap:
         try:
             self._iterable[-1], self._iterable[0] = self._iterable[0], self._iterable[-1]
             popped = self._iterable.pop()
+            return popped
         except IndexError:
             raise IndexError("pop from empty heap")
 
